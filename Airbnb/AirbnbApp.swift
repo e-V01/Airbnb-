ff064c303551ed7cd655034e465e9c7effd34b1f
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-16 17:41:39 +0300

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Y K on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
